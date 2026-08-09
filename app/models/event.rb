f2d7ef0047ce9ebae9e4284de61a6419758ac094
kaushikhande <kaushikhande302@gmail.com>
2026-08-09 22:17:21 +0530

    Added bellitto api rake tasks and validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,6 @@
 class Event < ApplicationRecord
+  validates :billetto_id, presence: true, uniqueness: true
+  validates :title, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
 end
